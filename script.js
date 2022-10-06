77ddadb573e49d8f7a743c18299a08b720d958ee
muchamadhatta <muchamadhatta96@gmail.com>
2022-10-06 09:34:03 +0700

    alert delete message

---
+++ script.js
@@ -0,0 +1,23 @@
+var SelectedRow = null;
+//Show Alerts
+function showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = 'alert alert-${className}';
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+}
+
+//Delete Data
+
+document.querySelector("#student-list").addEventListener("click", (e) => {
+    target = e.target;
+    if (target.classList.contains("delete")) {
+        target.parentElement.parentElement.remove();
+        showAlert("Student Data Deleted", "danger");
+    }
+});
