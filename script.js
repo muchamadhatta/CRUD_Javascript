1165fd35cb2f878f5c726cff30858fdebbf82f14
muchamadhatta <muchamadhatta96@gmail.com>
2022-11-18 08:52:25 +0700

    add data

--- script.js
+++ script.js
@@ -11,7 +11,49 @@ function showAlert(message, className) {
     setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
+// Clear All Fields
+function clearFields() {
+    document.querySelector("#firstName").value = "";
+    document.querySelector("#lastName").value = "";
+    document.querySelector("#rollNo").value = "";
 
+}
+
+//Add Data
+document.querySelector("#student-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    //Get Form Values
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const rollNo = document.querySelector("#rollNo").value;
+    //validate
+    if (firstName == "" || lastName == "" || rollNo == "") {
+        showAlert("Please fill in all fields", "danger");
+    }
+    else {
+        if (SelectedRow == null) {
+            const list = document.querySelector("#student-list");
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${rollNo}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+            <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
+            `;
+
+            list.appendChild(row);
+            SelectedRow = null;
+            showAlert("Student Added", "success");
+        } else {
+            SelectedRow.children[0].textContent = firstName;
+            SelectedRow.children[1].textContent = lastName;
+            SelectedRow.children[2].textContent = rollNo;
+
+        }
+    }
+});
 
 //Delete Data  b
 
